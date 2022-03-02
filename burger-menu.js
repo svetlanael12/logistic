289af12a42e663825355c392af94661a589a2968
Svetlana <svetlana_el12@mail.ru>
2022-03-02 12:25:53 +0500

    chore: regulate script

---
+++ burger-menu.js
@@ -0,0 +1,9 @@
+document.querySelector('.burger').addEventListener('click', function(){
+    document.querySelector('.burger span').classList.toggle('active');
+    document.querySelector('.menu').classList.toggle("animate");
+  })
+  
+ 
+
+
+
